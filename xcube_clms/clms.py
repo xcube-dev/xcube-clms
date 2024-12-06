3abef8e862ce27ef9374650b03c6f35ef87b76a9
Yogesh Kumar Baljeet Singh <yogesh09121995@gmail.com>
2024-12-06 10:38:08 +0100

    Refactor clms.py

--- xcube_clms/clms.py
+++ xcube_clms/clms.py
@@ -18,12 +18,11 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import inspect
 import os
-from typing import Any, get_args, Container
+from typing import Any, Container
 
 import xarray as xr
-from xcube.core.store import DataTypeLike
+from xcube.core.store import DataTypeLike, DataStore
 from xcube.util.jsonschema import JsonObjectSchema, JsonStringSchema
 
 from .constants import (
@@ -36,8 +35,6 @@ from .constants import (
     START_TIME_KEY,
     END_TIME_KEY,
     JSON_TYPE,
-    BATCH,
-    NEXT,
     SCHEMA_KEY,
     PROPERTIES_KEY,
     ALLOWED_SCHEMA_PARAMS,
@@ -57,18 +54,16 @@ from .utils import (
 
 class CLMS:
     def __init__(self, url: str, credentials: dict, path: str | None = None):
-        self._url = url
+        self._url: str = url
         self._datasets_info: list[dict[str, Any]] = []
         self._metadata: list[str] = []
-        self._data_ids = []
-        if path is None:
-            self.path = os.path.join(os.getcwd(), "preload_cache/")
-        else:
-            self.path = os.path.join(os.getcwd(), path)
+        self.path: str = os.path.join(os.getcwd(), path or "preload_cache/")
         self._preload_data = PreloadData(self._url, credentials, self.path)
-
-        self.file_store = self._preload_data.file_store
         self._fetch_all_datasets()
+
+    @property
+    def file_store(self) -> DataStore:
+        return self._preload_data.file_store
 
     def open_data(
         self,
@@ -82,25 +77,26 @@ class CLMS:
                 f"Expected a data_id in the format {{ product_id}}"
                 f"{DATA_ID_SEPARATOR}{{file_id}} but got {data_id}"
             )
-        if self.file_store is None:
+        if not self.file_store:
             raise ValueError(
                 "File store does not exist yet. Please preload "
                 "data first using the preload_data() method."
             )
+
         self._preload_data.refresh_cache()
-        if data_id in self._preload_data.cache.keys():
-            folder = self._preload_data.cache[data_id]
-            print(folder)
-            data_id_file = os.listdir(folder)
-            print(data_id_file)
-            if len(data_id_file) != 1:
-                LOG.error(
-                    f"Expected 1 file in the folder {folder}, "
-                    f"got {len(data_id_file)}. Opening the first file."
-                )
-            return self.file_store.open_data(
-                os.path.join(data_id, data_id_file[0]), **open_params
+        cache_entry = self._preload_data.cache.get(data_id)
+        if not cache_entry:
+            raise FileNotFoundError(f"No cached data found for data_id: {data_id}")
+
+        data_id_file = os.listdir(cache_entry)
+        if len(data_id_file) != 1:
+            LOG.warning(
+                f"Expected 1 file in the folder {cache_entry}, "
+                f"got {len(data_id_file)}. Opening the first file."
             )
+        return self.file_store.open_data(
+            os.path.join(data_id, data_id_file[0]), **open_params
+        )
 
     def get_data_ids(
         self,
@@ -111,14 +107,12 @@ class CLMS:
     def has_data(self, data_id: str, data_type: DataTypeLike = None) -> bool:
         if is_valid_data_type(data_type):
             dataset = self._get_item(data_id)
-            if len(dataset) == 0:
-                return False
-            return True
+            return bool(dataset)
         return False
 
-    def get_extent(self, data_id: str) -> dict:
+    def _get_extent(self, data_id: str) -> dict[str, Any]:
         item = self._access_item(data_id.split(DATA_ID_SEPARATOR)[0])
-        crs = item.get(CRS_KEY)
+        crs = item.get(CRS_KEY, [])
         time_range = (item.get(START_TIME_KEY), item.get(END_TIME_KEY))
 
         if len(crs) > 1:
@@ -126,7 +120,7 @@ class CLMS:
                 f"Expected 1 crs, got {len(crs)}. Outputting the first element."
             )
 
-        return dict(time_range=time_range, crs=crs[0])
+        return dict(time_range=time_range, crs=crs[0] if crs else None)
 
     def get_preload_params(self, data_id: str) -> dict[str : str | None]:
         self._fetch_all_datasets()
@@ -156,125 +150,76 @@ class CLMS:
         return schema
 
     def preload_data(self, *data_ids: str, **preload_params):
-        data_id_maps = {}
-        for data_id in data_ids:
-            item = self._access_item(data_id)
-            product = self._access_item(data_id.split(DATA_ID_SEPARATOR)[0])
-            data_id_maps[data_id] = {ITEM_KEY: item, PRODUCT_KEY: product}
-
+        data_id_maps = {
+            data_id: {
+                ITEM_KEY: self._access_item(data_id),
+                PRODUCT_KEY: self._access_item(data_id.split(DATA_ID_SEPARATOR)[0]),
+            }
+            for data_id in data_ids
+        }
         self._preload_data.initiate_preload(data_id_maps)
-        LOG.info(f"A local Filestore is created at the path: {self.path}")
+        LOG.info(f"Local Filestore created at the path: {self.path}")
 
     def _create_data_ids(
         self,
         include_attrs: Container[str] | bool | None = None,
     ) -> list[str] | list[tuple[str, dict[str, Any]]]:
-        if not self._datasets_info:
-            self._fetch_all_datasets()
-
         data_ids = []
         for item in self._datasets_info:
             for i in item[DOWNLOADABLE_FILES_KEY][ITEMS_KEY]:
                 if FILE_KEY in i and i[FILE_KEY] != "":
-                    if not include_attrs:
-                        data_ids.append(
-                            f"{item[CLMS_DATA_ID_KEY]}"
-                            f"{DATA_ID_SEPARATOR}{i[FILE_KEY]}"
-                        )
-                    elif isinstance(include_attrs, bool) and include_attrs:
+                    data_id = (
+                        f"{item[CLMS_DATA_ID_KEY]}{DATA_ID_SEPARATOR}{i[FILE_KEY]}"
+                    )
+                    if not include_attrs or (
+                        isinstance(include_attrs, bool) and include_attrs
+                    ):
+                        data_ids.append(data_id)
+                    elif isinstance(include_attrs, list):
+                        filtered_attrs = {k: i[k] for k in include_attrs if k in i}
                         data_ids.append(
                             (
                                 f"{item[CLMS_DATA_ID_KEY]}{DATA_ID_SEPARATOR}{i[FILE_KEY]}",
-                                i,
+                                filtered_attrs,
                             )
-                        )
-                    elif isinstance(include_attrs, list):
-                        attrs = {}
-                        for attr in include_attrs:
-                            if attr in i:
-                                attrs[attr] = i[attr]
-                        data_ids.append(
-                            (f"{item[CLMS_DATA_ID_KEY]}:{i[FILE_KEY]}", attrs)
                         )
         return data_ids
 
-    def _filter_dataset_attrs(
-        self, attrs: Container[str], raw_datasets: list[dict[str, Any]] = None
-    ) -> list[dict[str, Any]]:
-        """
-        Filters attributes of datasets in a list
-
-        Args:
-            attrs: A container of attribute names to filter for in each dataset.
-            raw_datasets: List of datasets metadata where each dataset contains the same keys
-
-        Returns:
-           A list of dictionaries with filtered keys.
-        """
-        if raw_datasets:
-            supported_keys = list(raw_datasets[0].keys())
-        else:
-            supported_keys = self._attrs
-
-        raw_datasets = raw_datasets or self._fetch_all_datasets()
-
-        signature = inspect.signature(self._filter_dataset_attrs)
-        if (attrs is not None and not isinstance(attrs, (list, set, tuple))) | (
-            attrs is None
-        ):
-            raise TypeError(
-                f"Expected instance "
-                f"{get_args(signature.parameters.get('attrs').annotation)}, "
-                f"got {type(attrs).__name__}"
-            )
-
-        return [
-            {key: item[key] for key in supported_keys if key in attrs}
-            for item in raw_datasets
-        ]
-
     def _fetch_all_datasets(self) -> list[dict[str, Any]]:
         if not self._datasets_info:
-            LOG.info(
-                f"Datasets not fetched yet. Fetching all datasets now from {self._url}"
-            )
+            LOG.info(f"Fetching datasets metadata from {self._url}")
 
             response_data = make_api_request(build_api_url(self._url, SEARCH_ENDPOINT))
-            while True:
+            while response_data:
                 response = get_response_of_type(response_data, JSON_TYPE)
                 self._datasets_info.extend(response.get(ITEMS_KEY, []))
-                next_page = response.get(BATCH, {}).get(NEXT)
-                if not next_page:
-                    break
-                response_data = make_api_request(next_page)
+                # next_page = response.get(BATCH, {}).get(NEXT)
+                # if not next_page:
+                #     break
+                # response_data = make_api_request(next_page)
+                response_data = make_api_request(response.get("next", None))
             self._attrs = list(self._datasets_info[0].keys())
         return self._datasets_info
 
     def _access_item(self, data_id) -> dict:
         dataset = self._get_item(data_id)
-        if len(dataset) > 1:
-            raise Exception(
-                f"Expected one item for data_id: {data_id}, found {len(dataset)}."
+        if len(dataset) != 1:
+            raise ValueError(
+                f"Expected one dataset for data_id: {data_id}, found"
+                f" {len(dataset)}."
             )
-        if len(dataset) == 0:
-            raise Exception(f"Data id: {data_id} not found in the CLMS catalog")
         return dataset[0]
 
     def _get_item(self, data_id):
-        if len(data_id.split(DATA_ID_SEPARATOR)) == 2:
+        if DATA_ID_SEPARATOR in data_id:
             clms_data_product_id, dataset_filename = data_id.split(DATA_ID_SEPARATOR)
-            dataset = []
-            for product in self._datasets_info:
-                for item in product.get(DOWNLOADABLE_FILES_KEY).get(ITEMS_KEY):
-                    if (
-                        item.get("file") == dataset_filename
-                        and product.get(CLMS_DATA_ID_KEY) == clms_data_product_id
-                    ):
-                        dataset.append(item)
-        else:
-            dataset = [
+            return [
                 item
-                for item in self._datasets_info
-                if data_id == item[CLMS_DATA_ID_KEY]
+                for product in self._datasets_info
+                if product[CLMS_DATA_ID_KEY] == clms_data_product_id
+                for item in product.get(DOWNLOADABLE_FILES_KEY, {}).get(ITEMS_KEY, [])
+                if item.get("file") == dataset_filename
             ]
-        return dataset
+        return [
+            item for item in self._datasets_info if data_id == item[CLMS_DATA_ID_KEY]
+        ]
